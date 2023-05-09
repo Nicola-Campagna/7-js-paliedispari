30c3f9c572c3f0a3190a58de78fe6c918961130c
Nicola-Campagna <campagnanicola379@gmail.com>
2023-05-09 11:37:05 +0200

    logica js pari e dispari

--- README.txt
+++ README.txt
@@ -3,6 +3,6 @@ Chiedere all'utente di inserire una parola
 Creare una funziona che verifichi se la parola inserita è palindroma,
 
 SNACK-2 : PARI E DISPARI
-Utente sceglie pari o dispari e inserisceun numero tra 1 e 5
+Utente sceglie pari o dispari e inserisce un numero tra 1 e 5
 Generiamo un numero random per il computer tra 1 e 5 usando una funzione
 Dichiariamo chi ha vinto
---
+++ pariedispari/main.js
@@ -0,0 +1,58 @@
+// utente sceglie pari o dispari
+const userChoice = prompt("PARI O DISPARI ?").toUpperCase();
+console.log(userChoice);
+
+// utente sceglie un n tra 1 e 5
+const userNumber = parseInt(prompt("INSERISCI UN NUMERO TRA 1 E 5"));
+console.log(userNumber + " :numero user");
+
+// numero assegnato al pc
+const pcNumber = generateRandomNumberOneToFive();
+console.log(pcNumber + " :numero pc");
+
+// somma tra numero utente e numero pc
+const somma = pcNumber + userNumber;
+console.log(somma + " :somma");
+
+
+// SE somma è un numero E la scelta dell'utente è una stringa E non una diverso da pari o dispari ...
+if (
+    !isNaN(somma) &&
+    somma <= 10 &&
+    String(userChoice) &&
+    userChoice === "PARI" || userChoice === "DISPARI"
+) {
+    console.log("valori inseriti correttamente");
+
+    // SE il n della somma è PARI e la scelta dell'utente è PARI -- VINCE UTENTE
+    if (isEven(somma) && userChoice === "PARI") {
+        console.log("HA VINTO UTENTE " + somma);
+    }
+    // ALTRIMENTI SE la somma è DISPARI e la scelta dell'utente è DISPARI --  VINCE UTENTE
+    else if (!isEven(somma) && userChoice === "DISPARI") {
+        console.log("HA VINTO UTENTE " + somma);
+    }
+    // ALTRIMENTI -- VINCE PC
+    else {
+        console.log("HA VINTO IL PC " + somma);
+    }
+
+}
+// ALTRIEMNTI ...
+else {
+    console.error("GIOCO INTERROTTO \n valori inseriti non corretti");
+}
+
+
+// setting funzione che genera un n random tra 1 e 5
+function generateRandomNumberOneToFive() {
+    const randomNumber = Math.floor(Math.random() * 5 + 1);
+    return randomNumber;
+}
+
+// setting funziona per verificare se un n è pari o dispari
+function isEven(num) {
+    num = parseInt(num);
+    // ritorna se il numero è pari
+    return num % 2 == 0;
+}
